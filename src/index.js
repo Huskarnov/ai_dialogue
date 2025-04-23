da75d1e0d975825bc3449a64fe8f175cc12dae38
huskarnov <143254754+Huskarnov@users.noreply.github.com>
2025-04-23 20:46:22 +0100

    initialized

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 //npm install -D webpack webpack-cli html-loader html-webpack-plugin css-loader style-loader webpack-dev-server webpack-merge
 
 import './styles.css';
+
+let gptAnswer;
+let geminiAnswer;
